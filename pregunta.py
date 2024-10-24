6de63126ca26fa2f1f250565bae3275b6f15bdf5
Tevin Barrios <BarriosTevin@gmail.com>
2024-10-24 09:33:41 -0700

    first commit

---
+++ pregunta.py
@@ -0,0 +1,5 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+average_grade = sum(grades) / len(grades)
+
+print(f"the average for my class is: {average_grade}")
---
+++ pregunta2.py
@@ -0,0 +1,7 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice is accounted for") 
+else: 
+    print("Alice is not here y'all something is fishy")
